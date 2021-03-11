d50a6452f56a6215510001671fc1bcf5d39236c6
Mike Zhong <mike.zhong89@gmail.com>
2021-03-10 22:29:59 -0500

    some additions

---
+++ jupyter_init.py
@@ -0,0 +1,10 @@
+from IPython.core.display import display, HTML
+import pandas as pd
+import os
+import sys
+import numpy as np
+
+
+display(HTML("<style>.container { width:80% !important; }</style>"))
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
